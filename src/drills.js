d3c9c48aa2deb34a958fa8140e3764a9a9f851ca
yl1462 <yliang1462@hotmail.com>
2021-01-20 19:58:12 -0600

    Databases with Node (CHK POINT 13)

---
+++ src/drills.js
@@ -0,0 +1,63 @@
+require('dotenv').config()
+const knex = require('knex')
+
+const knexInstance = knex({
+  client: 'pg',
+  connection: process.env.DB_URL
+})
+
+function searchByItemName(searchTerm) {
+  knexInstance
+    .select('*')
+    .from('shopping_list')
+    .where('name', 'ILIKE', `%${searchTerm}%`)
+    .then(result => {
+      console.log(result)
+    })
+}
+
+searchByItemName('Not Dogs')
+
+function paginateItems(pageNumber) {
+  const itemsPerPage = 6
+  const offset = itemsPerPage * (pageNumber - 1)
+  knexInstance
+    .select('*')
+    .from('shopping_list')
+    .limit(itemsPerPage)
+    .offset(offset)
+    .then(result => {
+      console.log(result)
+    })
+}
+
+paginateItems(2)
+
+function itemsAddedAfterDate(daysAgo) {
+  knexInstance
+    .select('*')
+    .from('shopping_list')
+    .where(
+      'date_added',
+      '>',
+      knexInstance.raw(`now() - '?? days'::INTERVAL`, daysAgo)
+    )
+    .then(result => {
+      console.log(result)
+    })
+}
+
+itemsAddedAfterDate(30)
+
+function totalCost() {
+  knexInstance
+    .select('category')
+    .sum('price as total')
+    .from('shopping_list')
+    .groupBy('category')
+    .then(result => {
+      console.log(result)
+    })
+}
+
+totalCost()
